df1f0eb12f718296c4a15eb61b06447b169a94b4
Matt Outen <mattoutenphysed@gmail.com>
2018-08-02 11:15:28 -0500

    touch and mkdir: complete

--- terminal-commands.js
+++ terminal-commands.js
@@ -1,10 +1,27 @@
 
 const fs = require('fs');
+
 module.exports.ls = () => {
     fs.readdir('./', (err, files) => {
     const filesToString = files.reduce((acc, file) => {
         return `${acc} ${file} `;
     }, '');
+
     console.log(filesToString);
+    });
+};
+
+module.exports.touch = () => {
+    fs.writeFile('message.txt', 'utf8', (err) => {
+        if (err) throw err;
+
+        console.log('file created')
+    });
+};
+
+module.exports.mkdir = () => {
+    fs.mkdir('./terminal-commands', (err, dir) => {
+        if (err.code != 'EEXIST') throw err;
+        console.log(`directory created`)
     });
 };
